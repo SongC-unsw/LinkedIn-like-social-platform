031c3a496a6ed0178af407e1fc95ff67be99e8fc
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-03-18 10:17:18 +1100

    Implement apiCall function for registration in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,8 +12,38 @@ const showPage = (chosenPage) => {
     document.querySelector(`.${chosenPage}.page`).classList.remove("hide");
     
 }
+const apiCall = (path, body) => {
+    // Method hardcoded as POST need improvement
+    return fetch('http://localhost:5005/' + path, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(body)
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) {
+        alert(data.error);
+      } else {
+        return Promise.resolve(data);
+        // or handle the data here
+      }
+    });
+  };
 showPage('registration');
 const submitBtn = document.getElementById("submit");
 submitBtn.addEventListener("click", () => {
-    fetch()
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    const password = document.getElementById('password').value;
+    const passConfirm = document.getElementById('confirmPassword').value;
+
+    const Data = {
+
+        email: email,
+        name: name,
+        password: password
+    };
+    apiCall("/auth/register",Data);
 })
