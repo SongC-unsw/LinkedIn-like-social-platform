d4b30e7d7d4dce3e8ef9e2dec2adeec1eff4a942
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-07-16 13:38:42 +1000

    feat: initialize frontend setup with Webpack configuration, package.json, and main entry point

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { BACKEND_PORT } from "./config.js";
 // A helper you may want to use when uploading new images to the server.
 import { fileToDataUrl } from "./helpers.js";
+import "../styles/global.css";
 
 const showPage = (chosenPage) => {
   const pages = document.querySelectorAll(".page");
