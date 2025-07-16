72a5c11e79a95ba442964235c46f1ce1c1991cf7
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-07-16 13:46:47 +1000

    chore: update Webpack configuration to change output directory from 'dist' to 'public'

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -29,14 +29,14 @@ module.exports = {
   ],
 
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"), // 改为 public
     filename: "bundle.js",
     clean: true,
   },
 
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "public"), // 改为 public
     },
     compress: true,
     port: 8080,
